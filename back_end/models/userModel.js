0a9a05bb033a550d8218d88e396ac0cc38fb32cf
hoang <hoangmp24@gmail.com>
2024-11-22 09:30:06 +0700

    commit

--- back_end/models/userModel.js
+++ back_end/models/userModel.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 // Định nghĩa schema cho người dùng
 const userSchema = new mongoose.Schema({
     userName: { type: String, required: true },
-    phoneNumber: { type: String, required: true, unique: true }, // Đảm bảo số điện thoại là duy nhất
+    phoneNumber: { type: String, required: true, unique: true }, 
     password: { type: String, required: true },
     diaChi: { type: String, required: true },
 }, {
